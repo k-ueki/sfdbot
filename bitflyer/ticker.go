82ef7a3ea3e36795318b9d722731294ad9cfd53b
ka28 <ku6.gatt@gmail.com>
2021-03-11 17:36:05 +0900

    [add] getTicker

--- bitflyer/ticker.go
+++ bitflyer/ticker.go
@@ -1,20 +1,34 @@
 package bitflyer
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/k-ueki/sfdbot/bitflyer/model"
 	"github.com/k-ueki/sfdbot/util"
 )
 
 const (
 	getTickerUrl = "https://lightning.bitflyer.com/api/trade/ticker/all?v=1"
+
+	codeBTCJPY = "BTC_JPY"
 )
 
-func GetTicker() (*model.Ticker, error) {
-	tickers, err := util.HttpGet(getTickerUrl)
+func GetTicker(code string) (*model.Ticker, error) {
+	val, err := util.HttpGet(getTickerUrl)
 	if err != nil {
 		return nil, err
 	}
 
-	resp := model.Ticker{}
-	return &resp, nil
+	tickers:=[]model.Ticker{}
+	if err:=json.Unmarshal(val,&tickers);err!=nil{
+		return nil,err
+	}
+
+	for _,ticker:=range tickers{
+		if ticker.ProductCode==code{
+			return &ticker,nil
+		}
+	}
+	return nil, errors.New(fmt.Sprintf("cannot find %v",code))
 }
