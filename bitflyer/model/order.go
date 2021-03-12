4196f74e818811771358274fb14c8b79a98c3b02
ka28 <ku6.gatt@gmail.com>
2021-03-12 13:35:38 +0900

    [add] order

--- bitflyer/model/order.go
+++ bitflyer/model/order.go
@@ -1,18 +1,31 @@
 package model
 
 type (
-	SimpleOrderRequest struct{
-		Code string `json:"product_code"`
-		Type string `json:"child_order_type"`
-		Side string `json:"side"`
-		Price float64 `json:"price"`
-		Size float64 `json:"size"`
-		Expire int64 `json:"minute_to_expire,omitempty"`
-		TimeInForce string `json:"time_in_force,omitempty"`
+	SimpleOrderRequest struct {
+		Code        string  `json:"product_code"`
+		Type        string  `json:"child_order_type"`
+		Side        string  `json:"side"`
+		Price       float64 `json:"price"`
+		Size        float64 `json:"size"`
+		Expire      int64   `json:"minute_to_expire,omitempty"`
+		TimeInForce string  `json:"time_in_force,omitempty"`
 	}
 
-	ShowOrderRequest struct{
+	ShowOrderRequest struct {
 		Code string `json:"product_code"`
-		ID string `json:"child_order_id"`
+		ID   string `json:"child_order_id"`
+	}
+
+	GetOrderResponse struct {
+		ID          int64   `json:"id"`
+		OrderID     string  `json:"child_order_id"`
+		Code        string  `json:"product_code"`
+		Side        string  `json:"side"`
+		Type        string  `json:"child_order_type"`
+		Price       float64 `json:"price"`
+		Size        float64 `json:"size"`
+		State       string  `json:"state"`
+		ExpiredDate string  `json:"expired_date"`
+		OrderDate   string  `json:"child_order_date"`
 	}
 )
--- bitflyer/order.go
+++ bitflyer/order.go
@@ -2,51 +2,68 @@ package bitflyer
 
 import (
 	"encoding/json"
-	"github.com/k-ueki/sfdbot/bitflyer/model"
+	"fmt"
 	"net/http"
+
+	"github.com/k-ueki/sfdbot/bitflyer/model"
 )
 
-const(
+const (
 	postSimpleOrderUrl = "/v1/me/sendchildorder"
+	getOrderUrl        = "/v1/me/getchildorders"
 
-	OrderTypeLimit = "LIMIT"
+	OrderTypeLimit  = "LIMIT"
 	OrderTypeMarket = "MARKET"
 
-	OrderSideBuy = "BUY"
+	OrderSideBuy  = "BUY"
 	OrderSideSell = "SELL"
 )
 
-func (api *APIClient)Execution(order model.SimpleOrderRequest)(*string,error){
-	body,err:=json.Marshal(order)
+func (api *APIClient) Execution(order model.SimpleOrderRequest) (*string, error) {
+	body, err := json.Marshal(order)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	resp,err:=api.Request(http.MethodPost, postSimpleOrderUrl,nil,body)
+	resp, err := api.Request(http.MethodPost, postSimpleOrderUrl, nil, body)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return getOrderID(resp),nil
+	return getOrderID(resp), nil
 }
 
 func (api *APIClient) Buy(orderType string, size float64) (*string, error) {
 	order := model.SimpleOrderRequest{
-		Code:    CodeBTCJPYFX,
-		Side:          OrderSideBuy,
+		Code: CodeBTCJPYFX,
+		Side: OrderSideBuy,
 		Type: orderType,
-		Size:           size,
+		Size: size,
 	}
 	return api.Execution(order)
 }
 
 func (api *APIClient) Sell(orderType string, size float64) (*string, error) {
 	order := model.SimpleOrderRequest{
-		Code:    CodeBTCJPYFX,
-		Side:           OrderSideSell,
+		Code: CodeBTCJPYFX,
+		Side: OrderSideSell,
 		Type: orderType,
-		Size:           size,
+		Size: size,
 	}
 	return api.Execution(order)
+}
+
+func (api *APIClient) GetOrderByID(id string) (*model.GetOrderResponse, error) {
+	val, err := api.Request(http.MethodGet, genGetOrderURLWithID(id), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := model.GetOrderResponse{}
+	if err := json.Unmarshal(val, resp); err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
 }
 
 func getOrderID(b []byte) *string {
@@ -57,4 +74,8 @@ func getOrderID(b []byte) *string {
 		return nil
 	}
 	return &resp.ChildOrderAcceptanceID
+}
+
+func genGetOrderURLWithID(id string) string {
+	return fmt.Sprintf(`%s?child_order_acceptance_id=%s&product_code=%s`, getOrderUrl, id, CodeBTCJPYFX)
 }
