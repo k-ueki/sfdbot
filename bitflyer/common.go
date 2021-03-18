65c1cd3c9a4552f9efb20bc3b2e773a56639f80a
ka28 <ku6.gatt@gmail.com>
2021-03-18 16:16:48 +0900

    request周り

--- bitflyer/common.go
+++ bitflyer/common.go
@@ -5,35 +5,44 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
-	"github.com/k-ueki/sfdbot/config"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/k-ueki/sfdbot/config"
 )
 
-const(
-	baseURL      = "https://api.bitflyer.com"
+const (
+	baseURL = "https://api.bitflyer.com"
 )
 
 type (
-	APIClient struct{
-		Key             string
-		Secret          string
+	APIClient struct {
+		Key    string
+		Secret string
 		Client *http.Client
 	}
 )
 
 func NewAPIClient() *APIClient {
-	c:=config.Config
+	c := config.Config
 	return &APIClient{
-		Key:             c.ApiKey,
-		Secret:          c.ApiSecret,
+		Key:    c.ApiKey,
+		Secret: c.ApiSecret,
 		Client: new(http.Client),
 	}
 }
 
 func (api *APIClient) Request(method, endpoint string, query map[string]string, body []byte) ([]byte, error) {
+	resp, err := api.RequestSimpleResponse(method, endpoint, query, body)
+	if err != nil {
+		return nil, err
+	}
+	return convertResponseToByte(resp)
+}
+
+func (api *APIClient) RequestSimpleResponse(method, endpoint string, query map[string]string, body []byte) (*http.Response, error) {
 	req, err := http.NewRequest(method, baseURL+endpoint, bytes.NewBuffer(body))
 	if err != nil {
 		return nil, err
@@ -53,15 +62,11 @@ func (api *APIClient) Request(method, endpoint string, query map[string]string, 
 		req.Header.Set("Content-Type", "application/json")
 	}
 
-	resp, err := api.Client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return respBytes, nil
+	return api.Client.Do(req)
+}
+
+func convertResponseToByte(resp *http.Response) ([]byte, error) {
+	return ioutil.ReadAll(resp.Body)
 }
 
 func (api *APIClient) getHeader(method, endpoint string, body []byte) map[string]string {
