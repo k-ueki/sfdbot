eea9ce6a9992c16f91c3df56269d9d05fe1dcec2
Katsuya Ueki <k.ueki@rb4.so-net.ne.jp>
2021-03-19 10:28:13 +0900

    [fix] logic

--- application/app.go
+++ application/app.go
@@ -12,40 +12,38 @@ func Start() error {
 	api := bitflyer.NewAPIClient()
 	preTicker := model.Ticker{}
 
-	for {
-		tickerCh := make(chan model.Ticker)
-		go bitflyer.GetTickerStream(bitflyer.CodeBTCJPY, tickerCh)
-		for ticker := range tickerCh {
-			//st := time.Now().UnixNano()
-			fmt.Println(ticker)
-			ltp := ticker.Ticker.Ltp
-			// Ltpが前のTickerと同じであれば無視
-			if preTicker.Ticker.Ltp == ticker.Ticker.Ltp {
-				break
-			}
+	tickerCh := make(chan model.Ticker)
+	go bitflyer.GetTickerStream(bitflyer.CodeBTCJPY, tickerCh)
+	for ticker := range tickerCh {
+		fmt.Println(ticker)
+		ltp := ticker.Ticker.Ltp
 
-			// positionの有無
-			positions, err := api.GetPosisionList()
-			if err != nil {
-				log.Fatal(err)
-				break
-			}
-			if len(positions) != 0 {
-				sfdPosition.Reset()
-			}
+		// positionの有無
+		positions, err := api.GetPosisionList()
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+		if len(positions) != 0 {
+			sfdPosition.Reset()
+		}
 
-			fmt.Println("sfdPosition: ", *sfdPosition.HaveOrder, sfdPosition.AcceptanceID)
+		fmt.Println("sfdPosition: ", *sfdPosition.HaveOrder, sfdPosition.AcceptanceID)
 
+		if preTicker.Ticker.Ltp != ltp {
 			if len(positions) == 0 {
 				//PositionがなければCancel & IFD指値
 				if sfdPosition.HaveOrder.Sell {
-					if err := api.Cancel(sfdPosition.AcceptanceID); err != nil {
+					if err := api.Cancel(model.CancelChildOrderRequest{
+						Code:              bitflyer.CodeFXBTCJPY,
+						OrderAcceptanceID: sfdPosition.AcceptanceID,
+					}); err != nil {
 						log.Fatal(err)
 						break
 					}
 				}
 
-				orderAcceptanceID, err := api.SellLimit(ltp * 1.05)
+				orderAcceptanceID, err := api.SellLimit(int64(ltp*1.05) + 2)
 				if err != nil {
 					log.Fatal(err)
 					break
@@ -68,7 +66,7 @@ func Start() error {
 					}
 				}
 
-				orderAcceptanceID, err := api.BuyLimit(ltp*1.05 - 10)
+				orderAcceptanceID, err := api.BuyLimit(int64(ltp*1.05) - 5)
 				if err != nil {
 					log.Fatal(err)
 					break
@@ -76,8 +74,6 @@ func Start() error {
 				sfdPosition.SetBuyOrder(*orderAcceptanceID)
 			}
 			preTicker = ticker
-			//fin := time.Now().UnixNano()
-			//fmt.Println(float64(fin-st) / 1000000000)
 		}
 	}
 
